d85bc992e260904fb767f2b93f38075a000757eb
Tarxos <ftarxos@gmail.com>
2023-09-05 23:11:52 +0300

    add app settings

--- RideCli/Program.cs
+++ RideCli/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text.Json;
+
+if (!Directory.Exists(Settings.ThundirePath)) Directory.CreateDirectory(Settings.ThundirePath);
+if (!Directory.Exists(Settings.LauncherPath)) Directory.CreateDirectory(Settings.LauncherPath);
+if (!File.Exists(Settings.SettingsPath)) File.Create(Settings.SettingsPath).Dispose();
+
+
+
+public static class Settings
+{
+	public static readonly string ThundirePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Thundire");
+	public static readonly string LauncherPath = Path.Combine(ThundirePath, "RideCli");
+	public static readonly string SettingsPath = Path.Combine(LauncherPath, "Settings.json");
+
+	public static AppSettings GetSettings()
+	{
+		var data = File.ReadAllText(SettingsPath);
+		AppSettings? appSettings = null;
+		if (data is { Length: > 2 })
+		{
+			appSettings = JsonSerializer.Deserialize<AppSettings>(data);
+		}
+		return appSettings ?? new AppSettings();
+	}
+
+	public static void Save(AppSettings paths)
+	{
+		File.WriteAllText(SettingsPath, JsonSerializer.Serialize(paths));
+	}
+}
+
+public class AppSettings
+{
+	public Dictionary<string, LaunchSetting> LaunchSettings { get; set; } = new();
+
+	public void Register(string alias, LaunchSetting settings)
+	{
+		if (LaunchSettings.ContainsKey(alias))
+		{
+			LaunchSettings[alias] = settings;
+			return;
+		}
+		LaunchSettings.TryAdd(alias, settings);
+	}
+
+	public LaunchSetting? Get(string alias)
+	{
+		if (LaunchSettings.TryGetValue(alias, out var setting)) return setting;
+		return default;
+	}
+}
+
+public record LaunchSetting(string Path, string Launcher, string Arguments);
