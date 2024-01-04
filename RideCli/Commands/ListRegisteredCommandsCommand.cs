6cfb22c1661b0b3b5f7402e84957028db485408f
Tarxos <ftarxos@gmail.com>
2024-01-05 00:18:56 +0300

    add command to list registered commands

---
+++ RideCli/Commands/ListRegisteredCommandsCommand.cs
@@ -0,0 +1,36 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace RideCli.Commands;
+
+internal sealed class ListRegisteredCommandsSettings : CommandSettings{
+	[CommandArgument(0, "[type]")] public ManagedCommands ManagedCommands { get; set; } = ManagedCommands.All;
+}
+
+internal sealed class ListRegisteredCommandsCommand : Command<ListRegisteredCommandsSettings> {
+	public override int Execute([NotNull] CommandContext context, [NotNull] ListRegisteredCommandsSettings settings) {
+		var appSettings = Settings.GetSettings();
+
+		if (settings.ManagedCommands is ManagedCommands.All or ManagedCommands.Launch) {
+			AnsiConsole.WriteLine("Launch commands:");
+			
+			foreach (var (alias, setting) in appSettings.LaunchSettings)
+            {
+				AnsiConsole.WriteLine($"""ride reg launch {alias} "{setting.Path}" "{setting.Launcher}" "{setting.Arguments}" """);
+            }
+        }
+		if (settings.ManagedCommands is ManagedCommands.All or ManagedCommands.Search) {
+			AnsiConsole.WriteLine("Search commands:");
+
+			foreach (var (alias, kindPath) in appSettings.KindPaths) {
+				AnsiConsole.WriteLine($"""ride reg search{(appSettings.SelectedKindPath == alias ? " -d" : "")} {alias} "{kindPath}" """);
+			}
+		}
+
+		return 0;
+	}
+}
+
+internal enum ManagedCommands { All, Launch, Search }
